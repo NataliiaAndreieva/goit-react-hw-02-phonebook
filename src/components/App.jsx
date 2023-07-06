d54a9bf80e05f3b733af9c7872339a2d0dda3eb5
NataliiaAndreieva <taliandreeva@gmail.com>
2023-07-06 21:37:34 +0300

    add form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,38 @@
 import React, { Component } from "react";
+import { nanoid } from 'nanoid'
 
 export class App extends Component {
   state = {
     contacts: [],
     name: '',
   };
-  return(
-    <div>
+
+  handelInputChange = event => {
+    this.setState({ name: event.currentTarget.value });
+  }
+
+  handelSubmit = e => {
+    e.preventDefault();
+
+    console.log(this.state);
+  }
+
+  render() { 
+
+  return (
+    <form onSubmit={this.handelSubmit}>
       <input
         type="text"
         name="name"
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
+        value={this.state.name}
+        onChange={this.handelInputChange}
       />
-    </div>
+      <button type="submit">Add contact</button>
+    </form>
   );
+  
+  }
 };
